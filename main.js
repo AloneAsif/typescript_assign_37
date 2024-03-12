709ebc013e80963db1700b541af7f9e9968e28f5
[AloneAsif] <[asifleon3@gmail.com]>
2024-03-12 17:59:29 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+"use strict";
+// Question 38 Cities
+function describe_city(city, country = 'unknown') {
+    console.log(`${city} is in ${country}`);
+}
+describe_city('karachi', 'pakistan');
+describe_city('egypt', 'cairo');
+describe_city('japan');
---
+++ main.ts
@@ -0,0 +1,10 @@
+// Question 38 Cities
+
+function describe_city(city:string, country:string = 'unknown'){
+    console.log(`${city} is in ${country}`)
+
+}
+
+describe_city('karachi','pakistan')
+describe_city('egypt','cairo')
+describe_city('japan')
